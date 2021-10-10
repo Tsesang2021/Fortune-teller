2614c64ec46f4c7a6431827ea7380db1a45f1b86
Tsesang <tsesangdolkar@Tsesangs-iMac.local>
2021-10-10 22:17:48 +0200

    Added prompt inside the function

--- ageCalculator.js
+++ ageCalculator.js
@@ -3,11 +3,16 @@ let futureYear = 2030;
 let ageCalculator = futureYear - birthYear;
 
 function age() {
+  let birthYear = prompt("What is your birth year?");
   if (birthYear < futureYear) {
-    alert(`${ageCalculator}`);
+    //alert(`${ageCalculator}`);
     alert("You can calculate  your age");
   } else if (ageCalculator < 0) {
     alert("Future year must be bigger than birth year");
+  }
+  //console.log(ageCalculator);
+  else {
+    return `You are ${ageCalculator} years old.`;
   }
   console.log(ageCalculator);
 }
